4ba1dcdcb9f9a1dd271ea7d1503930d917195479
hweeeeeei <hwei1233@163.com>
2018-12-24 17:46:17 +0800

    demos

---
+++ aop-demo-boot/src/main/java/com/example/aopdemo/AopDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.aopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopDemoApplication.class, args);
+
+        System.out.print("111111111111111111111");
+    }
+
+}
+
